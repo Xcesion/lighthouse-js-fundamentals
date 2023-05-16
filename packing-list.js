fd23bcc1bebf7a16baca21f940e2744902fa3ae6
Frank Shen <frank851207@gmail.com>
2023-05-16 16:31:08 -0700

    Iterating Over an Array With while

--- packing-list.js
+++ packing-list.js
@@ -1,3 +1,4 @@
+//*******************************************Iterating Over an Array With "for"*****************************************************
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
@@ -6,3 +7,12 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 console.log(packingList[3] + " see if the string console the 3rd element in the array");
+
+
+//*******************************************Iterating Over an Array With "while"***************************************************** */
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}

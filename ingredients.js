60124fbb0de1fe9bb5c93fc8529a42cd5c59668f
Frank Shen <frank851207@gmail.com>
2023-05-16 22:35:17 -0700

    ingredients challenge complete

--- ingredients.js
+++ ingredients.js
@@ -14,6 +14,6 @@ for (let i = 0; i < ingredients.length; i++) {
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
 
-for(let i = ingredients.length-1; i > 0; i--) {
+for(let i = ingredients.length-1; i >= 0; i--) {
   console.log(ingredients[i]);
 }
